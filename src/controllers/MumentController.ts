2696e6fdfb0a34a18683f1e211436eac9463f036
kimchaeeun3447 <rlacodms3447@naver.com>
2022-07-12 22:00:05 +0900

    [FEAT] 뮤멘트 기록하기 구현 #4
    - PostBaseResponseDto 생성
    - 뮤멘트 기록하기 Controller, DTO, Service 구현
    - response message 추가

--- src/controllers/MumentController.ts
+++ src/controllers/MumentController.ts
@@ -2,6 +2,33 @@ import express, { Request, Response } from "express";
 import statusCode from "../modules/statusCode";
 import message from "../modules/responseMessage";
 import util from "../modules/util";
-import { UserService } from "../services";
+import { MumentService } from "../services";
+import { MumentCreateDto } from "../interfaces/mument/MumentCreateDto";
+import { PostBaseResponseDto } from "../interfaces/common/PostBaseResponseDto";
 
-export default {}
+/**
+ *  @route POST /mument/:userId/:musicId
+ *  @desc Create Mument
+ */
+const createMument = async (req: Request, res: Response) => {
+    const mumentCreateDto: MumentCreateDto = req.body;
+    const { userId, musicId } = req.params;
+    
+    if (!userId || !musicId ) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.BAD_REQUEST));
+
+    try {
+        const data: PostBaseResponseDto | null = await MumentService.createMument(userId, musicId, mumentCreateDto);
+
+        if (!data) return res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, message.NOT_FOUND));
+
+        res.status(statusCode.CREATED).send(util.success(statusCode.CREATED, message.CREATE_MUMENT_SUCCESS, data));
+    } catch (error) {
+        console.log(error);
+        
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+    }
+};
+
+export default {
+    createMument,
+}
--- src/modules/responseMessage.ts
+++ src/modules/responseMessage.ts
@@ -2,7 +2,16 @@ const message = {
     NULL_VALUE: '필요한 값이 없습니다.',
     NOT_FOUND: '존재하지 않는 자원',
     BAD_REQUEST: '잘못된 요청',
-    INTERNAL_SERVER_ERROR: '서버 내부 오류'
+    INTERNAL_SERVER_ERROR: '서버 내부 오류',
+
+    // user
+
+
+    // mument
+    CREATE_MUMENT_SUCCESS: '뮤멘트 기록하기 성공',
+
+    
+    // music
 }
 
 export default message;
