0cacaa17dfdafdb675cdeea87924ed7f50a7a368
kimchaeeun3447 <rlacodms3447@naver.com>
2022-07-12 15:20:45 +0900

    [ADD] db.ts에 createcollection 추가

--- src/loaders/db.ts
+++ src/loaders/db.ts
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 import config from "../config";
+import Like from "../models/Like";
+import Mument from "../models/Mument";
+import Music from "../models/Music";
+import User from "../models/User";
 
 const connectDB = async () => {
     try {
@@ -7,6 +11,23 @@ const connectDB = async () => {
         mongoose.set('autoCreate', true);
 
         console.log("Mongoose Connected ...");
+
+        User.createCollection().then(function (collection) {
+            console.log("User Collection is created!!");
+        });
+
+        Music.createCollection().then(function (collection) {
+            console.log("Music Collection is created!!");
+        });
+
+        Mument.createCollection().then(function (collection) {
+            console.log("Mument Collection is created!!");
+        });
+
+        Like.createCollection().then(function (collection) {
+            console.log("Like Collection is created!!");
+        });
+
     } catch (err: any) {
         console.error(err.message);
         process.exit(1);
