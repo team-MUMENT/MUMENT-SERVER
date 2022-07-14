b3a83d51c33e65b8202724f31e60a4361d722a96
kimchaeeun3447 <rlacodms3447@naver.com>
2022-07-14 18:32:03 +0900

    [CHORE] lint 적용 #9

--- src/interfaces/mument/MumentInfo.ts
+++ src/interfaces/mument/MumentInfo.ts
@@ -1,22 +1,22 @@
 import mongoose from 'mongoose';
 
 export interface MumentInfo {
-    music: {
-        _id: mongoose.Types.ObjectId;
-    };
-    user: {
-        _id: mongoose.Types.ObjectId;
-        name: string;
-        image: string;
-    };
-    isFirst: boolean;
-    impressionTag: number[];
-    feelingTag: number[];
-    content: string;
-    isPrivate: boolean;
-    likeCount: number;
-    isDeleted: boolean;
-    createdAt?: Date;
+  music: {
+    _id: mongoose.Types.ObjectId;
+  };
+  user: {
+    _id: mongoose.Types.ObjectId;
+    name: string;
+    image: string;
+  };
+  isFirst: boolean;
+  impressionTag: number[];
+  feelingTag: number[];
+  content: string;
+  isPrivate: boolean;
+  likeCount: number;
+  isDeleted: boolean;
+  createdAt?: Date;
 }
 
 export interface UserMumentInfo {
