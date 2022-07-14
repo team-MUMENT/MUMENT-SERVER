8250b8025b7ee70baa8723955e21b7e8dffd8613
kimchaeeun3447 <rlacodms3447@naver.com>
2022-07-14 18:27:18 +0900

    [CHORE] eslint 적용 #6

--- src/interfaces/user/UserInfo.ts
+++ src/interfaces/user/UserInfo.ts
@@ -1,15 +1,15 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 export interface UserInfo {
-    _id?: mongoose.Types.ObjectId;
-    profileId: string,
-    password: string,
-    name: string,
-    image: string
+  _id?: mongoose.Types.ObjectId;
+  profileId: string;
+  password: string;
+  name: string;
+  image: string;
 }
 
 export interface UserIdInfo {
-    _id: mongoose.Types.ObjectId;
-    image: string;
-    name: string;
+  _id: mongoose.Types.ObjectId;
+  image: string;
+  name: string;
 }
