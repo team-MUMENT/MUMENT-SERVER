d535ddc87feaf709a73dab2a15bfe6993519b692
Youjung Heo <hujumee27@gmail.com>
2022-07-15 23:29:11 +0900

    [MOD] 라우터 파라미터 길이 검증 추가 #7

--- src/routes/MusicRouter.ts
+++ src/routes/MusicRouter.ts
@@ -4,6 +4,9 @@ import { body, param } from 'express-validator';
 
 const router: Router = Router();
 
-router.get('/:musicId/:userId', [param('musicId').isLength(24), param('userId').isLength(24)], MusicController.getMusicAndMyMument);
+router.get('/:musicId/:userId', [
+    param('musicId').isString().isLength({min: 24, max: 24}), 
+    param('userId').isString().isLength({min: 24, max: 24}),
+], MusicController.getMusicAndMyMument);
 
 export default router;
