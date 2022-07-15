16157d2c975571279a35556178c2fb3349b41fa9
Youjung Heo <hujumee27@gmail.com>
2022-07-15 16:27:09 +0900

    [MOD] document 구조분해 및 res 코드 수정 #7

--- src/controllers/MusicController.ts
+++ src/controllers/MusicController.ts
@@ -3,7 +3,6 @@ import statusCode from '../modules/statusCode';
 import message from '../modules/responseMessage';
 import util from '../modules/util';
 import { validationResult } from 'express-validator';
-import { UserService } from '../services';
 import { MusicService } from '../services';
 
 /**
@@ -11,26 +10,25 @@ import { MusicService } from '../services';
  * @DESC 곡 상세보기 뷰에서 music 정보와 나의 뮤멘트 정보 가져오기
  */
 const getMusicAndMyMument = async (req: Request, res: Response) => {
-  const { musicId, userId } = req.params;
-  const error = validationResult(req);
+    const { musicId, userId } = req.params;
+    const error = validationResult(req);
 
-  if (!error.isEmpty()) {
-      return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.WRONG_PARAMS));
-  };
-
-
-  try {
-    const data = await MusicService.getMusicAndMyMument(musicId, userId);
-    
-    if (!data) {
-        return res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, message.NOT_FOUND));
+    if (!error.isEmpty()) {
+        res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.WRONG_PARAMS));
     }
 
-    res.status(statusCode.OK).send(util.success(statusCode.OK, message.FIND_MUSIC_MYMUMENT_SUCCESS, data));
-  } catch (error) {
-    console.log(error);
-    return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.SERVICE_UNAVAILABLE, message.INTERNAL_SERVER_ERROR));
-  }
+    try {
+        const data = await MusicService.getMusicAndMyMument(musicId, userId);
+
+        if (!data) {
+            res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, message.NOT_FOUND));
+        }
+
+        res.status(statusCode.OK).send(util.success(statusCode.OK, message.FIND_MUSIC_MYMUMENT_SUCCESS, data));
+    } catch (error) {
+        console.log(error);
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.SERVICE_UNAVAILABLE, message.INTERNAL_SERVER_ERROR));
+    }
 };
 
 export default {
