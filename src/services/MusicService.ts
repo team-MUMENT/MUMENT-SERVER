6f03457606274357ef185b5635cdb3e0bf8e32bd
Youjung Heo <hujumee27@gmail.com>
2022-07-16 01:29:34 +0900

    [FEAT] 뮤멘트 조회 service 구현 #12

--- src/services/MusicService.ts
+++ src/services/MusicService.ts
@@ -1,12 +1,14 @@
 import dayjs from 'dayjs';
+import { MumentInfo } from '../interfaces/mument/MumentInfo';
+import { MusicMumentListResponseDto } from '../interfaces/music/MusicMumentListResponseDto';
 
-import { MyMumentResponseDto } from '../interfaces/mument/MyMumentResponseDto';
+import { MusicMyMumentResponseDto } from '../interfaces/music/MusicMyMumentResponseDto';
 import Like from '../models/Like';
 import Mument from '../models/Mument';
 import Music from '../models/Music';
 
 // 곡 상세보기 - 음악, 나의 뮤멘트 조회
-const getMusicAndMyMument = async (musicId: string, userId: string): Promise<MyMumentResponseDto | null> => {
+const getMusicAndMyMument = async (musicId: string, userId: string): Promise<MusicMyMumentResponseDto | null> => {
     try {
         // 곡 조회
         const music = await Music.findById(musicId);
@@ -62,6 +64,53 @@ const getMusicAndMyMument = async (musicId: string, userId: string): Promise<MyM
     }
 };
 
+const getMumentList = async(musicId: string, userId: string, isLikeOrder: boolean): Promise<MusicMumentListResponseDto | null> => {    
+    try {
+        // 있는 곡인지 조회
+        const music = await Music.findById(musicId);
+        if (!music) return null;
+
+        let originalMumentList: MumentInfo [];
+
+        switch (isLikeOrder) {
+            case (true): {
+                // 좋아요순 정렬
+                originalMumentList = await Mument.find({
+                   'music._id': musicId,
+                   isDeleted: false, 
+                }).sort({
+                    'likeCount': -1
+                });
+            }
+            case (false): {
+                // 최신순 정렬
+                originalMumentList = await Mument.find({
+                    'music._id': musicId,
+                    isDeleted: false,
+                }).sort({
+                    'createdAt': -1
+                });
+            }
+
+            const createDate = (createdAt: string): string => {
+                const date = dayjs(createdAt).format('D MMM, YYYY');
+                return date;
+            };
+
+            const getIsLiked = (mumentId: string, userId: string) 
+
+            function 
+
+            const mumentList: MusicMumentListResponseDto[] = originalMumentList.map(()
+
+            )
+        }
+
+
+    }
+}
+
 export default {
     getMusicAndMyMument,
+    getMumentList,
 };
