460a73fb557b20623ce94ac5dd85f47efe3263d5
Youjung Heo <hujumee27@gmail.com>
2022-07-17 05:55:10 +0900

    [FIX] 타입에러 수정 #13

--- src/routes/MumentRouter.ts
+++ src/routes/MumentRouter.ts
@@ -9,7 +9,7 @@ router.post('/:userId/:musicId', MumentController.createMument);
 router.get('/:mumentId/:userId', MumentController.getMument);
 
 // 히스토리 조회
-router.get(':userId/:musicId/history', [
+router.get('/:userId/:musicId/history', [
     param('musicId').isString().isLength({ min: 24, max: 24 }),
     param('userId').isString().isLength({ min: 24, max: 24 }),
     query('default').isString().isIn(['Y', 'N']),
