4b4b78b324127f476cad5fad73dd89aec27ea3e9
Youjung Heo <hujumee27@gmail.com>
2022-07-17 03:45:00 +0900

    [FEAT] 히스토리 조회 라우터 #13

--- src/routes/MumentRouter.ts
+++ src/routes/MumentRouter.ts
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 import { MumentController } from '../controllers';
+import { body, param, query } from 'express-validator';
+
 
 const router: Router = Router();
 
 router.post('/:userId/:musicId', MumentController.createMument);
 router.get('/:mumentId/:userId', MumentController.getMument);
+
+// 히스토리 조회
+router.get(':userId/:musicId/history', [
+    param('musicId').isString().isLength({ min: 24, max: 24 }),
+    param('userId').isString().isLength({ min: 24, max: 24 }),
+    query('default').isString().isIn(['Y', 'N']),
+], MumentController.getMumentHistory);
 
 export default router;
