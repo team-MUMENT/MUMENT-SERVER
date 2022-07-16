8d74c2e3662046a4fb517878ac7cf6b2894d6123
Youjung Heo <hujumee27@gmail.com>
2022-07-17 04:14:24 +0900

    [ADD] 뮤멘트 카드뷰 인터페이스 추가 #16

---
+++ src/interfaces/mument/MumentCardViewInterface.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+export interface MumentCardViewInterface {
+    _id: mongoose.Types.ObjectId;
+    music: {
+        _id: mongoose.Types.ObjectId;
+    };
+    user: {
+        _id: mongoose.Types.ObjectId;
+        name: string;
+        image: string;
+    };
+    isFirst: boolean;
+    impressionTag: number[];
+    feelingTag: number[];
+    content: string;
+    isPrivate: boolean;
+    likeCount: number;
+    isDeleted: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+    __v: number;
+    date: string;
+    isLiked: boolean;
+}
