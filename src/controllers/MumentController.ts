e8d061db6dc9800ae7b60144176e5857e71ebe3f
Youjung Heo <hujumee27@gmail.com>
2022-07-17 16:53:52 +0900

    [FEAT] 좋아요 기능 controller 로직 구현 #21

--- src/controllers/MumentController.ts
+++ src/controllers/MumentController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import statusCode from '../modules/statusCode';
 import message from '../modules/responseMessage';
 import util from '../modules/util';
+import constant from '../modules/serviceReturnConstant';
 import { validationResult } from 'express-validator';
 import { MumentService } from '../services';
 import { MumentCreateDto } from '../interfaces/mument/MumentCreateDto';
@@ -107,12 +108,19 @@ const createLike = async (req: Request, res: Response) => {
     try {
         const data = await MumentService.createLike(mumentId, userId);
 
-        // 업데이트가 안됐거나, musicId가 없을 때
-        if (!data) {
-            res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.BAD_REQUEST));
+        // 실패했을 때
+        switch (data) {
+            case null: {
+                // 업데이트가 실패했을 때
+                res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.CREATE_LIKE_FAIL));
+            }
+            case constant.NO_MUSIC: {
+                // 음악 data가 없을 때
+                res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.NO_MUSIC_ID));
+            }
         }
 
-        res.status(statusCode.OK).send(util.success(statusCode.OK, message.CREATE_LIKE_SUCCESS, data));
+        res.status(statusCode.CREATED).send(util.success(statusCode.CREATED, message.CREATE_LIKE_SUCCESS, data));
     } catch (error) {
         console.log(error);
         res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
@@ -134,7 +142,11 @@ const deleteLike = async (req: Request, res: Response) => {
     try {
         const data = await MumentService.deleteLike(mumentId, userId);
 
-        res.status(statusCode.OK).send(util.success(statusCode.OK, message.CREATE_MUMENT_SUCCESS, data));
+        if (!data) {
+            res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.DELETE_LIKE_FAIL));
+        }
+
+        res.status(statusCode.OK).send(util.success(statusCode.OK, message.DELETE_LIKE_SUCCESS, data));
 
     } catch (error) {
         console.log(error);
