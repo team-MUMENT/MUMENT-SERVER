e86ffa107fe71f586bd7b63dc65d8a8744884c81
kimchaeeun3447 <rlacodms3447@naver.com>
2022-07-18 06:28:00 +0900

    [FEAT] 뮤멘트 수정하기 구현 #27
    - Controller, Service, DTO

--- src/controllers/MumentController.ts
+++ src/controllers/MumentController.ts
@@ -30,6 +30,34 @@ const createMument = async (req: Request, res: Response) => {
 };
 
 /**
+ *  @ROUTE PUT /mument/:mumentId
+ *  @DESC Update Mument
+ */
+const updateMument = async (req: Request, res: Response) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.BAD_REQUEST));
+    }
+
+    const { mumentId } = req.params;
+    const mumentUpdateDto: MumentCreateDto = req.body;
+
+    try {
+        const data = await MumentService.updateMument(mumentId, mumentUpdateDto);
+
+        if (!data) {
+            res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, message.NO_MUMENT_ID));
+        }
+
+        res.status(statusCode.OK).send(util.success(statusCode.OK, message.UPDATE_MUMENT_SUCCESS, data));
+    } catch (error) {
+        console.log(error);
+
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+    }
+};
+
+/**
  *  @ROUTE GET /:mumentId/:userId
  *  @DESC Get Mument
  */
@@ -177,6 +205,7 @@ const deleteLike = async (req: Request, res: Response) => {
 
 export default {
     createMument,
+    updateMument,
     getMument,
     getIsFirst,
     getMumentHistory,
