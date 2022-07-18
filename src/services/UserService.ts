cf6af610f787f0bd06433ec873c79e64adb49314
kimchaeeun3447 <rlacodms3447@naver.com>
2022-07-18 17:18:38 +0900

    [FEAT] 보관함 - 카드뷰 태그 개수 처리 #33
    나의 뮤멘트 리스트

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -52,6 +52,21 @@ const getMyMumentList = async (userId: string, tagList: number[]): Promise<UserM
                     ],
                 });
 
+                const impressionTagLength = mument.impressionTag.length;
+                const feelingTagLength = mument.feelingTag.length;
+
+                if (impressionTagLength >= 1 && feelingTagLength >= 1) {
+                    mument.impressionTag = [mument.impressionTag[0]];
+                    mument.feelingTag = [mument.feelingTag[0]];
+                    console.log(mument.impressionTag, mument.feelingTag);
+                } else if (impressionTagLength >= 1 && feelingTagLength < 1) {
+                    mument.impressionTag = mument.impressionTag.slice(0, 2);
+                    console.log(mument.impressionTag.slice(0, 2));
+                } else if (impressionTagLength < 1 && feelingTagLength >= 1) {
+                    mument.feelingTag = mument.feelingTag.slice(0, 2);
+                    console.log(mument.impressionTag.slice(0, 2));
+                }
+
                 const result: MumentResponseDto = {
                     _id: mument._id,
                     user: {
