9ce30bac1b012d5238bdb3c09a63a103033f2247
Youjung Heo <hujumee27@gmail.com>
2022-07-19 01:53:38 +0900

    [FEAT] 랜덤 뮤멘트 response dto 생성 #30

---
+++ src/interfaces/mument/RandomMumentResponeDto.ts
@@ -0,0 +1,18 @@
+export interface RandomMumentResponseDto {
+    title: string;
+    mumentList: {
+        _id: string;
+        music: {
+            name: string;
+            artist: string;
+        };
+        user: {
+            name: string;
+            image?: string;
+        };
+        impressionTag: number[];
+        feelingTag: number[];
+        content: string;
+        createdAt: Date;
+    }[];
+};
