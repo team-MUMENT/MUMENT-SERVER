026874f0581702eefe4edc9e0326de9d7a534eb4
Youjung Heo <hujumee27@gmail.com>
2022-07-20 04:51:02 +0900

    [REFACTOR] auth 라우터 분리

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import UserRouter from './UserRouter';
 import MumentRouter from './MumentRouter';
 import MusicRouter from './MusicRouter';
+import AuthRouter from './AuthRouter';
 
 const router = Router();
 
 router.use('/user', UserRouter);
 router.use('/mument', MumentRouter);
 router.use('/music', MusicRouter);
+router.use('/auth', AuthRouter);
 
 export default router;
