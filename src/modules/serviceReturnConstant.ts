853b7bf7f2becfb9b2a311377eefb9aecc24a264
Youjung Heo <hujumee27@gmail.com>
2022-07-21 07:42:24 +0900

    [CHORE] 홈 컨텐트가 없을 경우 상수 추가

--- src/modules/serviceReturnConstant.ts
+++ src/modules/serviceReturnConstant.ts
@@ -7,6 +7,7 @@ const constant = {
     NO_MUSIC: -2, // 아이디로 조회한 음악의 값이 없을 때
     NO_MUMENT: -3, // 아이디로 조회한 뮤멘트의 값이 없을 때
     WRONG_PASSWORD: -4, // 패스워드가 일치하지 않을 때
+    NO_HOME_CONTENT: -5, // 홈에 들어갈 뮤멘트가 존재하지 않을 때
 };
 
 export default constant;
