863d6c3fdfe849d34f2b464bce5c73476a251359
Youjung Heo <hujumee27@gmail.com>
2022-07-22 00:29:59 +0900

    [FEAT] 다시 들은 곡 뮤멘트 조회 성공 #51

--- src/modules/responseMessage.ts
+++ src/modules/responseMessage.ts
@@ -34,6 +34,7 @@ const message = {
     RANDOM_TAG_FAIL: '랜덤 태그 생성 실패',
     GET_TODAY_MUMENT_SUCCESS: '오늘의 뮤멘트 리스트 조회 성공',
     GET_BANNER_SUCCESS: '배너 조회 성공',
+    GET_AGAIN_MUMENT_SUCCESS: '다시 들은 곡 뮤멘트 리스트 조회 성공',
 
     // music
     NO_MUSIC_ID: '해당 아이디의 음악이 존재하지 않습니다',
