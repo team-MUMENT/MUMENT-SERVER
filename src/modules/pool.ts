4fcf8db08ae2cfc19c22facd5cb69db79109a408
kimchaeeun3447 <rlacodms3447@naver.com>
2022-08-15 05:42:34 +0900

    [ADD] query 모듈 추가 #63
    value가 필요한 query 전달 시 사용할 모듈 추가

--- src/modules/pool.ts
+++ src/modules/pool.ts
@@ -19,12 +19,12 @@ const query = async (query: string) => {
                 const result = await connection.query(query);
               
                 //Connection 할당 해제
-                connection.release(connection);
+                connection.release();
                 
                 //결과 반환
                 resolve(result);
             } catch (err) {
-                connection.release(connection);
+                connection.release();
                 reject(err);
             }
         } catch (err) {
@@ -34,9 +34,29 @@ const query = async (query: string) => {
 };
 
 // // query에 필요한 value를 따로 받을 시 
-// const queryValue = async (query: string, value: any) => {
-    
-// };
+const queryValue = async (query: string, value: any) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            //Connection Pool 생성
+            const pool = await poolPromise;
+
+            //Connection 생성
+            const connection = await pool.getConnection();
+
+            try {
+                // value와 함께 query 실행
+                const result = await connection.query(query, value);
+                connection.release();
+                resolve(result);
+            } catch (err) {
+                connection.release();
+                reject(err);
+            }
+        } catch (err) {
+            reject(err);
+        }
+    });
+};
 
 // // 트랜잭션 필요 시
 // const transaction =async (...args: any[]) => {
@@ -45,6 +65,6 @@ const query = async (query: string) => {
 
 export default {
     query,
-    // queryValue,
+    queryValue,
     // transaction
 };
