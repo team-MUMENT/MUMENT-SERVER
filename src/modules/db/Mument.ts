ce8eed9404e5212319d183e02fa8cd16194c2867
kimchaeeun3447 <rlacodms3447@naver.com>
2022-11-18 01:35:50 +0900

    [ADD] 뮤멘트 좋아요 유무 판단 모듈 Mument.ts에 분리 #73
    - any 사용부분 타입 지정하도록 수정도 함

--- src/modules/db/Mument.ts
+++ src/modules/db/Mument.ts
@@ -1,5 +1,12 @@
-/* mument 리소스 - 재사용 쿼리 - 트랜잭션 쓸 때 사용가능 */  
 import { ExistMumentDto } from "../../interfaces/mument/ExistMumentRDBDto";
+import { NumberBaseResponseDto } from "../../interfaces/common/NumberBaseResponseDto";
+import pools from '../pool';
+import { MumentInfoRDB } from "../../interfaces/mument/MumentInfoRdb";
+
+/**
+ * mument 관련 재사용 쿼리 - 트랜잭션 쓸 때 사용가능
+ */
+
 
 // 뮤멘트 태그 삽입 - impressionTag, feelingTag 리스트 합쳐서 처리
 const mumentTagCreate = async (impressionTag: number[], feelingTag: number[], connection: any, mumentId: string) => {
@@ -18,7 +25,7 @@ const mumentTagCreate = async (impressionTag: number[], feelingTag: number[], co
 // 존재하는 뮤멘트 id인지 판단
 const isExistMument = async (mumentId: string , connection: any) => {
     const query = 'SELECT * FROM mument WHERE id=? AND NOT is_deleted=1;'; //삭제되지 않은 뮤멘트여야 함
-    const mument: any[] = await connection.query(query, [mumentId]);
+    const mument: MumentInfoRDB[] = await connection.query(query, [mumentId]);
 
     return mument.length === 0 ? false : true; // 존재하지 않으면 false 반환
 };
@@ -27,7 +34,7 @@ const isExistMument = async (mumentId: string , connection: any) => {
 // 존재하는 뮤멘트 id인지 판단 & 뮤멘트 정보 반환
 const isExistMumentInfo = async (mumentId: string , connection: any): Promise<ExistMumentDto> => {
     const query = 'SELECT * FROM mument WHERE id=? AND NOT is_deleted=1;'; //삭제되지 않은 뮤멘트여야 함
-    const mument: any[] = await connection.query(query, [mumentId]);
+    const mument: MumentInfoRDB[] = await connection.query(query, [mumentId]);
     
     return {
         isExist: mument.length === 0 ? false : true, // 존재하지 않으면 false
@@ -35,8 +42,28 @@ const isExistMumentInfo = async (mumentId: string , connection: any): Promise<Ex
     };
 };
 
+
+
+/**
+ * mument관련 재사용 쿼리 - 트랜잭션 없이 사용가능
+ */
+
+// userId가 해당 뮤멘트에 좋아요를 눌렀는지 확인
+const isLiked = async (mumentId: string, userId: string) => {
+    const query = 'SELECT EXISTS(SELECT * FROM mument.like WHERE mument_id=? AND user_id=?) as exist;';
+
+    // 좋아요 존재하면 1, 존재하지 않으면 0 반환함
+    const isLiked: NumberBaseResponseDto[] = await pools.queryValue(query, [mumentId, userId]);
+    console.log(isLiked);
+    //console.log(isLiked.exist);
+
+    return isLiked[0].exist;
+};
+
+
 export default {
     mumentTagCreate,
     isExistMument,
     isExistMumentInfo,
+    isLiked,
 }
