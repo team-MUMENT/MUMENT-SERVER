d4552385766a28356e5bb082be8f539b7649b43c
Youjung Heo <hujumee27@gmail.com>
2022-12-08 06:07:46 +0900

    [ADD] 카카오 로그인 라이브러리 access token으로 카카오 프로파일 조회하는 코드 추가 #79

--- src/library/kakaoAuth.ts
+++ src/library/kakaoAuth.ts
@@ -1,14 +1,36 @@
 const axios = require('axios').default;
 
-//const request = require('https');
-const kakaoRequestUrl = 'kapi.kakao.com/v2/user/me?secure_resource=true';
-
 /**
- * @DESC 클라에게 받은 액세스 토큰으로 사용자 정보 가져오기
- * @ERR
- * 1. 액세스 토큰으로 요청한 사용자 정보가 없을 때
+ * @DESC 클라에게 받은 authentication code로 토큰 발급 받기
  */
-const getKakaoProfile = async (kakaoAccesstoken: string) => {
-    return axios.post('kapi.kakao.com'
-    );
+const getKakaoToken = async (authenticationCode: string) => {
+    return axios({
+        method: 'post',
+        url: 'https://kauth.kakao.com/oauth/token',
+        headers: {
+            'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+        },
+        data: {
+            'grant_type': 'authorization_code',
+            'client_id': process.env.REST_API_APP_KEY,
+            'redirect_uri': process.env.REDIRECT_URI,
+            'code': authenticationCode
+        }
+    });
+};
+
+const getKakaoProfile = (kakaoAccessToken: string) => {
+    return axios({
+        method: 'post',
+        url: 'https://kapi.kakao.com/v2/user/me',
+        headers: {
+            'Authorization': 'Bearer ' + kakaoAccessToken,
+            'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+        }
+    });
+};
+
+export default {
+    getKakaoToken,
+    getKakaoProfile
 }
