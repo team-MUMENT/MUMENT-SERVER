63fd6a267d34de408649f7c6aea3d6a1c69d38fd
kimchaeeun3447 <rlacodms3447@naver.com>
2022-12-11 21:36:31 +0900

    [ADD] 내가 작성한 뮤멘트 검색 쿼리 처리 모듈 - mument, tag, music 테이블 join #76

--- src/modules/db/User.ts
+++ src/modules/db/User.ts
@@ -1,5 +1,8 @@
 import { UserInfoRDB } from '../../interfaces/user/UserInfoRDB';
 import pools from '../pool';
+import { MumentInfoRDB } from "../../interfaces/mument/MumentInfoRdb";
+import mumentDB from './Mument';
+import { MyMumentInfoRDB } from '../../interfaces/mument/MyMumentInfoRDB';
 
 /**
  * user 관련 재사용 쿼리 - 트랜잭션 쓸 때 사용가능
@@ -20,7 +23,37 @@ const userInfo = async (userId: string) => {
     return user[0];
 }
 
+// 내가 작성한 뮤멘트 리스트 가져오기 - 최신순
+const myMumentList = async (userId: string) => {
+    interface mumentAndLikeInfo {
+        mument: MyMumentInfoRDB;
+        LikeCount: number;
+    }
+    let myMumentLikeCountList = [];
+
+    // mument & mument tag & music 테이블 조인
+    const mumentListQuery = `
+        SELECT mument.id as mument_id, user_id,
+            music_id, is_first, like_count,
+            is_private, mument.created_at as created_at,
+            artist, image, name, tag_id
+            FROM mument
+            JOIN music
+            ON mument.music_id = music.id
+            JOIN mument_tag
+            ON mument.id = mument_tag.mument_id
+            WHERE user_id=${userId} AND mument.is_deleted=0
+            ORDER BY mument.created_at DESC;`;
+    
+    const myMumentList: MyMumentInfoRDB[] = await pools.query(mumentListQuery);
+    console.log(myMumentList);
+
+    return myMumentList;
+};
+
+
 export default {
     userInfo,
+    myMumentList
 }
 
