4f0cfcc5644bf77ab2873ef7bcfde70e01ce44e3
Youjung Heo <hujumee27@gmail.com>
2022-12-23 21:55:56 +0900

    [DEL] 카카오 로그인 쿼리 삭제 #81

--- src/modules/db/Auth.ts
+++ src/modules/db/Auth.ts
@@ -1,13 +1,2 @@
 import pools from '../pool';
 import { UserInfoRDB } from '../../interfaces/user/UserInfoRDB';
-
-
-// 카카오 토큰 획득 후 db에 해당 사용자 칼럼 추가
-const createKakaoUser = async (authenticationCode: string, email?: string, gender?: string, ageRange?: string) => {
-    const query = `
-    INSERT INTO user (provider, authentication_code, email, gender, age_range)
-    VALUE (kakao, ${authenticationCode},${email}, ${gender}, ${ageRange});
-    `;
-
-    await pools.query(query);
-};
