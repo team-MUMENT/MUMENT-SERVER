24b655b43dc85cbce5df5c8f235b6d11e77436cd
Youjung Heo <hujumee27@gmail.com>
2022-12-23 19:58:18 +0900

    [CHORE] user 테이블 스키마 변경 반영 #81

--- src/interfaces/user/UserInfoRDB.ts
+++ src/interfaces/user/UserInfoRDB.ts
@@ -1,10 +1,15 @@
 // RDB에서 SELECT한 user 레코드 타입 정의
 export interface UserInfoRDB {
     id: number;
+    provider: string;
     name: string;
     profile_id: string;
     image: string;
     is_deleted: number;
     created_at: Date;
     updated_at: Date;
+    authentication_code: string;
+    gender: string;
+    age_render: string;
+    email: string;
 }
