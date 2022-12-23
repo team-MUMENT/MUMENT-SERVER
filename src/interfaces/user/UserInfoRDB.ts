be6e05211d8f26406430f94672ab3d3d248cab58
Youjung Heo <hujumee27@gmail.com>
2022-12-23 21:51:08 +0900

    [DEL] not null이 아닌 칼럼 ?처리 #81

--- src/interfaces/user/UserInfoRDB.ts
+++ src/interfaces/user/UserInfoRDB.ts
@@ -2,14 +2,14 @@
 export interface UserInfoRDB {
     id: number;
     provider: string;
-    name: string;
-    profile_id: string;
-    image: string;
+    name?: string;
+    profile_id?: string;
+    image?: string;
     is_deleted: number;
     created_at: Date;
     updated_at: Date;
-    authentication_code: string;
-    gender: string;
-    age_render: string;
-    email: string;
+    authentication_code?: string;
+    gender?: string;
+    age_render?: string;
+    email?: string;
 }
