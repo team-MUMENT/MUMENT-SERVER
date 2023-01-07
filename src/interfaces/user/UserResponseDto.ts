198c2a7ed5998b045faf4387e0ce3fb29afcb1fb
kimchaeeun3447 <rlacodms3447@naver.com>
2023-01-08 02:29:12 +0900

    [CHORE] 유저 response DTO RDB에 맞게 수정 #94

--- src/interfaces/user/UserResponseDto.ts
+++ src/interfaces/user/UserResponseDto.ts
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
 export interface UserResponseDto {
-    id: mongoose.Types.ObjectId;
+    id: mongoose.Types.ObjectId | string;
     profileId: string;
-    name: string;
+    name?: string; // RDB에서 name 필드 제거했기 때문에 옵셔널로 수정함
     image?: string;
 }
