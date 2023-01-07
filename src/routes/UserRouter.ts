4d773a8aa39f9278092876896d01a573346c7b26
kimchaeeun3447 <rlacodms3447@naver.com>
2023-01-08 02:28:09 +0900

    [FEAT] 차단 유저 리스트 API 라우터, 컨트롤러, 서비스 로직 작성 #94

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -8,5 +8,6 @@ const router: Router = Router();
 router.get('/my/list', auth, UserController.getMyMumentList);
 router.get('/like/list', auth, UserController.getLikeMumentList);
 router.post('/block/:mumentId', auth, UserController.blockUser);
+router.get('/block', auth, auth, UserController.getBlockedUserList);
 
 export default router;
