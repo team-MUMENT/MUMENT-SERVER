aeb41f91e17d3d730edc8589a5a482ea04380b0a
kimchaeeun3447 <rlacodms3447@naver.com>
2023-01-14 21:24:39 +0900

    CHORE: 소식창 response 메시지 추가, 필요없는 주석 제거  #106

--- src/modules/responseMessage.ts
+++ src/modules/responseMessage.ts
@@ -20,6 +20,8 @@ const message = {
     SELF_BLOCK: '자기자신은 차단할 수 없습니다',
     READ_BLOCK_LIST: '차단 유저 조회 성공',
     DELETE_BLOCKED_USER_SUCCESS: '유저 차단 해제 성공',
+    READ_NEWS_LIST_SUCCESS: '소식창 리스트 조회 성공',
+
 
     // mument
     NO_MUMENT_ID: '해당 아이디의 뮤멘트가 존재하지 않습니다',
