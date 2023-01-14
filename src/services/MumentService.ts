5e42b42a9b8880bccb4606a7bed786e794e232bd
kimchaeeun3447 <rlacodms3447@naver.com>
2023-01-15 03:48:47 +0900

    [CHORE]: 트랜잭션 시작 문에 await 붙이기, 중복된 곳 제거 #106

--- src/services/MumentService.ts
+++ src/services/MumentService.ts
@@ -48,7 +48,6 @@ import { NoticeInfoRDB } from '../interfaces/mument/NoticeInfoRDB';
 const createMument = async (userId: string, musicId: string, mumentCreateDto: MumentCreateDto): Promise<PostBaseResponseDto | null> => {
     const pool: any = await poolPromise;
     const connection = await pool.getConnection();
-    connection.beginTransaction(); //롤백을 위해 필요함
 
     try {    
         await connection.beginTransaction(); // 트랜잭션 적용 시작
@@ -91,7 +90,6 @@ const createMument = async (userId: string, musicId: string, mumentCreateDto: Mu
 const updateMument = async (mumentId: string, mumentUpdateDto: MumentCreateDto): Promise<StringBaseResponseDto | null | number> => {
     const pool: any = await poolPromise;
     const connection = await pool.getConnection();
-    connection.beginTransaction(); //롤백을 위해 필요함
 
     try {
         await connection.beginTransaction(); // 트랜잭션 적용 시작
@@ -139,7 +137,6 @@ const updateMument = async (mumentId: string, mumentUpdateDto: MumentCreateDto):
 const getMument = async (mumentId: string, userId: string): Promise<MumentResponseDto | null | number> => {
     const pool: any = await poolPromise;
     const connection = await pool.getConnection();
-    connection.beginTransaction(); //롤백을 위해 필요함
     
     try {
         // 존재하지 않는 id의 뮤멘트를 조회하려고 할 때
@@ -213,7 +210,6 @@ const getMument = async (mumentId: string, userId: string): Promise<MumentRespon
 const deleteMument = async (mumentId: string): Promise<void | null> => {
     const pool: any = await poolPromise;
     const connection = await pool.getConnection();
-    connection.beginTransaction(); //롤백을 위해 필요함
 
     try {
         await connection.beginTransaction(); // 트랜잭션 적용 시작
@@ -459,7 +455,6 @@ const getMumentHistory = async (userId: string, musicId: string, writerId: strin
 const createLike = async (mumentId: string, userId: string): Promise<LikeCountResponeDto | null | number> => {
     const pool: any = await poolPromise;
     const connection = await pool.getConnection();
-    connection.beginTransaction(); //롤백을 위해 필요함
 
     try {
         await connection.beginTransaction();
@@ -523,7 +518,6 @@ const createLike = async (mumentId: string, userId: string): Promise<LikeCountRe
 const deleteLike = async (mumentId: string, userId: string): Promise<LikeCountResponeDto | null | number> => {
     const pool: any = await poolPromise;
     const connection = await pool.getConnection();
-    connection.beginTransaction(); //롤백을 위해 필요함
     
     try {
         await connection.beginTransaction();
@@ -811,9 +805,10 @@ const getNoticeList = async (): Promise<NoticeInfoRDB[]> => {
 const createReport = async (mumentId: string, reportCategory: number[], etcContent: string, userId: string): Promise<void | number> => {
     const pool: any = await poolPromise;
     const connection = await pool.getConnection();
-    connection.beginTransaction(); //롤백을 위해 필요함
     
     try {
+        await connection.beginTransaction(); //롤백을 위해 필요함
+
         // 신고 당하는 유저 id 가져오기
         const reportedMument = await mumentDB.isExistMumentInfo(mumentId, connection);
         let reportedUser: number;
