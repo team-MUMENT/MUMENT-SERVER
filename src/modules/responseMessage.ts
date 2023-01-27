775795a12524b081f72be39ab670d9e0502e80f2
Youjung Heo <hujumee27@gmail.com>
2023-01-28 05:07:46 +0900

    [MOD] 리프래쉬 토큰값이 없는 경우 400 리턴 추가 #122

--- src/modules/responseMessage.ts
+++ src/modules/responseMessage.ts
@@ -73,6 +73,7 @@ const message = {
     APPLE_TOKEN_UNAUTHORIZED: '애플 API 개발자 토큰이 유효하지 않습니다',
     APPLE_SERVER_INTERNAL_ERROR: '애플 API 서버 자체 에러',
     NO_AUTHENTICATION_CODE: 'authentication code값이 없습니다',
+    NO_KAKAO_REFRESH_TOKEN: '카카오 리프래쉬 토큰값이 없습니다',
     NOT_CORRECT_TOKEN: 'refresh token이 일치하지 않습니다',
     RENEW_ACCESS_TOKEN: 'access token 갱신 성공',
     RENEW_ACCESS_REFRESH_TOKEN: 'access token과 refresh token 갱신 성공',
