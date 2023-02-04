73a9252205174b80ca88037e1c5163d00d27624d
kimchaeeun3447 <rlacodms3447@naver.com>
2023-02-05 03:38:07 +0900

    [FEAT] 곡 상세보기+나의 뮤멘트 API - DB에 없는 곡 일 경우 저장 후 반환하도록 로직 추가 #145

--- src/interfaces/mument/MumentCreateDto.ts
+++ src/interfaces/mument/MumentCreateDto.ts
@@ -1,11 +1,9 @@
-export interface MumentCreateDto {
+import { MusicCreateDto } from "../music/MusicCreateDto";
+
+export interface MumentCreateDto extends MusicCreateDto {
     isFirst: boolean;
     impressionTag: number[];
     feelingTag: number[];
     content?: string;
     isPrivate?: boolean;
-    musicId?: string;
-    musicArtist?: string;
-    musicImage?: string;
-    musicName?: string;
 }
