bf7b9c837fa57503d770a3510134974387bde3fc
kimchaeeun3447 <rlacodms3447@naver.com>
2023-02-05 21:44:09 +0900

    [CHORE] 문의하기 구글 폼 URL 추가 #145

--- src/modules/db/WebviewLink.ts
+++ src/modules/db/WebviewLink.ts
@@ -11,8 +11,8 @@ const privacy = 'https://destiny-plum-124.notion.site/45172b6e18e44ec0b97a65ff2d
 // 자주묻는 질문 -> 아직 링크없어서 이용약관으로 대체!
 const faq = 'https://destiny-plum-124.notion.site/55167e54a5a94da1898e45f654c03299';
 
-// 문의하기 -> 아직 링크없어서 이용약관으로 대체!
-const contact = 'https://destiny-plum-124.notion.site/55167e54a5a94da1898e45f654c03299';
+// 문의하기 
+const contact = 'https://docs.google.com/forms/d/e/1FAIpQLSfUS-k64fjUzNElSp4hEuLkcjZm5r2tR4428VaNbGc_IDx4Vw/viewform';
 
 // 앱 정보 -> 아직 링크없어서 이용약관으로 대체!
 const appInfo = 'https://destiny-plum-124.notion.site/55167e54a5a94da1898e45f654c03299';
