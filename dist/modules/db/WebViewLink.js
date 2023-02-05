9c57e00291ec56233cb5f46c93c50469436d7a92
kimchaeeun3447 <rlacodms3447@naver.com>
2023-02-05 22:38:49 +0900

    [CHORE] 웹뷰 링크 URL 추가 #145

--- dist/modules/db/WebViewLink.js
+++ dist/modules/db/WebViewLink.js
@@ -7,12 +7,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const tos = 'https://destiny-plum-124.notion.site/55167e54a5a94da1898e45f654c03299';
 // 개인정보처리방침
 const privacy = 'https://destiny-plum-124.notion.site/45172b6e18e44ec0b97a65ff2d528f2f';
-// 자주묻는 질문 -> 아직 링크없어서 이용약관으로 대체!
-const faq = 'https://destiny-plum-124.notion.site/55167e54a5a94da1898e45f654c03299';
+// 자주묻는 질문 
+const faq = 'https://destiny-plum-124.notion.site/318c4f5609b94d24babb6e3b25fdd46a';
 // 문의하기 
 const contact = 'https://docs.google.com/forms/d/e/1FAIpQLSfUS-k64fjUzNElSp4hEuLkcjZm5r2tR4428VaNbGc_IDx4Vw/viewform';
-// 앱 정보 -> 아직 링크없어서 이용약관으로 대체!
-const appInfo = 'https://destiny-plum-124.notion.site/55167e54a5a94da1898e45f654c03299';
+// 앱 정보
+const appInfo = 'https://destiny-plum-124.notion.site/1e307b334f7649c4b38444fa783c4ff8';
 // 뮤멘트 소개
 const introduction = 'https://destiny-plum-124.notion.site/Team-Mument-0e435810c49342318ed1dff405455b69';
 exports.default = {
