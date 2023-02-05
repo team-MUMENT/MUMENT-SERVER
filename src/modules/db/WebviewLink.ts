8d23879693c5dfc31ccdb3df8479ae558fafd3a0
kimchaeeun3447 <rlacodms3447@naver.com>
2023-02-05 21:17:49 +0900

    [FEAT] 웹 뷰 링크 조회 API 라우팅, 컨트롤러, 서비스 로직 구현 #145

---
+++ src/modules/db/WebviewLink.ts
@@ -0,0 +1,31 @@
+/**
+ * 로그인, 마이페이지 뷰 웹 뷰 링크
+ */
+
+// 이용약관
+const tos = 'https://destiny-plum-124.notion.site/55167e54a5a94da1898e45f654c03299';
+
+// 개인정보처리방침
+const privacy = 'https://destiny-plum-124.notion.site/45172b6e18e44ec0b97a65ff2d528f2f';
+
+// 자주묻는 질문 -> 아직 링크없어서 이용약관으로 대체!
+const faq = 'https://destiny-plum-124.notion.site/55167e54a5a94da1898e45f654c03299';
+
+// 문의하기 -> 아직 링크없어서 이용약관으로 대체!
+const contact = 'https://destiny-plum-124.notion.site/55167e54a5a94da1898e45f654c03299';
+
+// 앱 정보 -> 아직 링크없어서 이용약관으로 대체!
+const appInfo = 'https://destiny-plum-124.notion.site/55167e54a5a94da1898e45f654c03299';
+
+// 뮤멘트 소개
+const introduction = 'https://destiny-plum-124.notion.site/Team-Mument-0e435810c49342318ed1dff405455b69';
+
+
+export default {
+    tos,
+    privacy,
+    faq,
+    contact,
+    appInfo,
+    introduction,
+}
