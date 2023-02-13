c04deeed72086d4d02422940d78df8af324eac53
kimchaeeun3447 <rlacodms3447@naver.com>
2023-02-13 21:28:31 +0900

    [ADD] 마이페이지 오픈소스 라이센스 노션 조회 추가 #168

--- src/modules/db/WebviewLink.ts
+++ src/modules/db/WebviewLink.ts
@@ -20,6 +20,8 @@ const appInfo = 'https://destiny-plum-124.notion.site/1e307b334f7649c4b38444fa78
 // 뮤멘트 소개
 const introduction = 'https://destiny-plum-124.notion.site/Team-Mument-0e435810c49342318ed1dff405455b69';
 
+// 오픈소스 라이선스
+const license = "https://destiny-plum-124.notion.site/73e77fe21ecd4aa3a26d6237166d60d2";
 
 export default {
     tos,
@@ -28,4 +30,5 @@ export default {
     contact,
     appInfo,
     introduction,
+    license,
 }
