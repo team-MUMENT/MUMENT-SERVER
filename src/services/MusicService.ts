49e791e48c870a348fe354ce4b3a0d5f36adca0d
Youjung Heo <hujumee27@gmail.com>
2023-02-14 17:20:08 +0900

    [MOD] 곡 상세보기에서 나의 비밀글도 보이도록 수정 #177

--- src/services/MusicService.ts
+++ src/services/MusicService.ts
@@ -196,10 +196,10 @@ const getMumentList = async (musicId: string, userId: string, isLikeOrder: boole
                     AND mument.user_id NOT IN ${strBlockUserList}
                     AND mument.is_deleted = 0  
                     AND user.is_deleted = 0
-                    AND mument.is_private = 0
+                    AND is_private = 0 OR (user.id = ? AND is_private = 1)
                 ORDER BY mument.like_count DESC;
                 `;
-                originalMumentList = await connection.query(getMumentListQuery, [musicId]);
+                originalMumentList = await connection.query(getMumentListQuery, [musicId, userId]);
 
                 break;
             } case false: { // 최신순 정렬
@@ -212,10 +212,10 @@ const getMumentList = async (musicId: string, userId: string, isLikeOrder: boole
                     AND mument.user_id NOT IN ${strBlockUserList}
                     AND mument.is_deleted = 0  
                     AND user.is_deleted = 0
-                    AND mument.is_private = 0
+                    AND is_private = 0 OR (user.id = ? AND is_private = 1)
                 ORDER BY mument.created_at DESC;
                 `;
-                originalMumentList = await connection.query(getMumentListQuery, [musicId]);
+                originalMumentList = await connection.query(getMumentListQuery, [musicId, userId]);
 
                 break;
             }
