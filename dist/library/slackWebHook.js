8938733d68273a84540abe5fdb9819f75897427b
Youjung Heo <hujumee27@gmail.com>
2023-03-01 21:46:13 +0900

    [ADD] build 파일 추가

--- dist/library/slackWebHook.js
+++ dist/library/slackWebHook.js
@@ -14,23 +14,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
 const config_1 = __importDefault(require("../config"));
-<<<<<<< HEAD
-const API_URL = config_1.default.webhookURI;
-=======
 let API_URL = config_1.default.webhookURI;
 if (process.env.NODE_ENV == 'production') {
     API_URL = config_1.default.webhookReleaseURI;
 }
 const API_REPORT_URL = config_1.default.webhookReportURI; // 기획_서버_신고접수 채널
->>>>>>> 6f65d3d7d8e4a839810a0352a17b768d376a3557
 const slackErrorMessage = (errorStack) => {
     return {
         title: 'MUMENT ec2 서버 오류',
         text: '서버 내부 오류입니다',
-<<<<<<< HEAD
-=======
         type: 'error',
->>>>>>> 6f65d3d7d8e4a839810a0352a17b768d376a3557
         fields: [
             {
                 title: 'Error Stack:',
@@ -39,14 +32,6 @@ const slackErrorMessage = (errorStack) => {
         ],
     };
 };
-<<<<<<< HEAD
-// 슬랙 api url과 연결하는 함수
-const getChannels = () => {
-    return {
-        production: API_URL,
-    };
-};
-=======
 const slackPushFailMessage = (errorStack) => {
     return {
         title: '푸시 실패자 알림',
@@ -86,7 +71,6 @@ const getChannels = (type) => {
         };
     }
 };
->>>>>>> 6f65d3d7d8e4a839810a0352a17b768d376a3557
 // 슬랙 알림 보내기
 const sendMessage = (message) => __awaiter(void 0, void 0, void 0, function* () {
     if (!message) {
@@ -106,11 +90,7 @@ const sendMessage = (message) => __awaiter(void 0, void 0, void 0, function* () 
     }
     data.attachments.push(message);
     (0, axios_1.default)({
-<<<<<<< HEAD
-        url: getChannels().production,
-=======
         url: getChannels(message.type).production,
->>>>>>> 6f65d3d7d8e4a839810a0352a17b768d376a3557
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -118,9 +98,5 @@ const sendMessage = (message) => __awaiter(void 0, void 0, void 0, function* () 
         data,
     });
 });
-<<<<<<< HEAD
-exports.default = { sendMessage, slackErrorMessage };
-=======
 exports.default = { sendMessage, slackErrorMessage, slackPushFailMessage, slackReportMessage };
->>>>>>> 6f65d3d7d8e4a839810a0352a17b768d376a3557
 //# sourceMappingURL=slackWebHook.js.map
